d4485598b3d37d3829473691f68f399876cff2ff
M B Ashish <mbashish007@gmail.com>
2024-11-28 01:44:51 +0530

    Added User entity for storing username and password for login

---
+++ ERP/src/main/java/edu/academia/ERP/entity/User.java
@@ -0,0 +1,25 @@
+package edu.academia.ERP.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "username", unique = true ,nullable = false)
+    private String username;
+
+    @Column(name="password", nullable = false)
+    private String password;
+}
