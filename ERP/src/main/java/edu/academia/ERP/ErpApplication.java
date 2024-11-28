b54c8a7456ad2653c467b2e943e0dd134af2da8b
M B Ashish <mbashish007@gmail.com>
2024-11-28 10:23:06 +0530

    ERP Application

---
+++ ERP/src/main/java/edu/academia/ERP/ErpApplication.java
@@ -0,0 +1,13 @@
+package edu.academia.ERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpApplication.class, args);
+	}
+
+}
