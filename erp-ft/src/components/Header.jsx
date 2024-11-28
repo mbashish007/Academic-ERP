9879a8a58f3312e75be4e6e46e8458dad3e68417
M B Ashish <mbashish007@gmail.com>
2024-11-29 00:10:11 +0530

    added button to redirect to /domain route

--- erp-ft/src/components/Header.jsx
+++ erp-ft/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useAuth from '../hooks/useAuth';
+import { Link } from 'react-router-dom';
 
 const Header = ({isloggedIn}) => {
 
@@ -9,9 +10,18 @@ console.log(token);
   return  ( 
     <header className="sticky top-0 bg-blue-800 text-white p-4 flex justify-between items-center shadow-lg w-full">
       <div className="text-xl font-bold">Academic ERP</div>
-      {isloggedIn && ( <button onClick={logout} 
+     
+      
+      {isloggedIn && ( 
+         <div>
+        <Link to="/domain" className="bg-white hover:bg-blue-100 text-blue-900 font-bold py-2 px-4 rounded mr-3">Domain</Link>
+
+        <button onClick={logout} 
       className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" > Logout 
-      </button> )}
+      </button> 
+       </div>)
+      }
+     
     </header>
   );
 };
